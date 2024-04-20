6b2fbe16b080fd70f0d91313c994cae321650e3e
Ramanjulu1234 <ramanjulu7890@gmail.com>
2024-04-20 17:52:53 +0530

    backend

--- backend.sh
+++ backend.sh
@@ -50,3 +50,11 @@ VALIDATE $? "Creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
 VALIDATE $? "Downloading backend code"
+
+cd /app
+rm -rf /app/*
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracted backend code"
+
+npm install &>>$LOGFILE
+VALIDATE $? "Installing nodejs dependencies"
